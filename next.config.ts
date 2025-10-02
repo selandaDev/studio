5d84ab6a25c6ec5d864d3033b77f53a05d009072
fran garcia <fmanosa23@gmail.com>
2025-10-02 17:09:08 +0000

    fill

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'https://media.themoviedb.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
